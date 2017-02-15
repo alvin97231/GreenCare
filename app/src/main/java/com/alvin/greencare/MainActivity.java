4525481848925b2c93f0e897e7c802c9f41d323c
alvin97231 <alvin.eustache@gmail.com>
2017-02-15 17:09:59 +0100

    Add sign in test

--- app/src/main/java/com/alvin/greencare/MainActivity.java
+++ app/src/main/java/com/alvin/greencare/MainActivity.java
@@ -2,19 +2,31 @@ package com.alvin.greencare;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.alvin.greencare.Auth.*;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String TAG = "Main";
 
     private static final Class[] CLASSES = new Class[]{
             GoogleSignInActivity.class,
@@ -27,18 +39,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     };
 
     @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up ListView and Adapter
-        ListView listView = (ListView) findViewById(R.id.list_view);
+        mAuth = FirebaseAuth.getInstance();
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
 
-        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
-        adapter.setDescriptionIds(DESCRIPTION_IDS);
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.main_layout);
+                    Button button = new Button(MainActivity.this);
+                    layout.addView(button);
+                    button.setText(user.getDisplayName());
 
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+
+                    // Set up ListView and Adapter
+                    ListView listView = (ListView) findViewById(R.id.list_view);
+
+                    MyArrayAdapter adapter = new MyArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_2, CLASSES);
+                    adapter.setDescriptionIds(DESCRIPTION_IDS);
+
+                    listView.setAdapter(adapter);
+                    listView.setOnItemClickListener(MainActivity.this);
+                }
+            }
+        };
+        // [END auth_state_listener]
     }
 
     @Override
