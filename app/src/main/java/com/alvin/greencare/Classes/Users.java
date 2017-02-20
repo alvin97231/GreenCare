2c3f4b65c646794fe6770f5e0248de863dfb8a95
alvin97231 <alvin.eustache@gmail.com>
2017-02-20 17:09:25 +0100

    Services and db Finish user service Start Db Service and realtime features

--- app/src/main/java/com/alvin/greencare/Classes/Users.java
+++ app/src/main/java/com/alvin/greencare/Classes/Users.java
@@ -1,11 +1,14 @@
 package com.alvin.greencare.Classes;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 /**
  * Created by Alvin on 17/02/2017.
@@ -13,16 +16,68 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Users {
 
-    private FirebaseUser user;
     private static final String TAG = "UsersService";
 
-    private void updateUserPassword(FirebaseUser user, String newPassword) {
+    private void updateProfil(String displayName, String photoUri, FirebaseUser user){
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(displayName)
+                .setPhotoUri(Uri.parse(photoUri))
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                        }
+                    }
+                });
+    }
+
+    private void updatePassword(FirebaseUser user, String newPassword) {
         user.updatePassword(newPassword)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "User password updated.");
+                        }
+                    }
+                });
+    }
+
+    private void updateEmail(FirebaseUser user, String emailAddress){
+        user.updateEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User email address updated.");
+                        }
+                    }
+                });
+    }
+
+    private void sendEmailVerfication(FirebaseUser user){
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+    }
+
+    private void passwordReset(FirebaseAuth auth, String emailAddress){
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
                         }
                     }
                 });
